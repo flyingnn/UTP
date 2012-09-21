81cbbbacac12428ade2827391dc95058dc6f8436
flyingnn <flyingnn@gmail.com>
2012-09-21 16:47:18 +0800

    增加后台登录验证码功能.

--- application/views/login/login_view.php
+++ application/views/login/login_view.php
@@ -11,32 +11,50 @@
 <div class="" style="width:400px;margin:100px auto;">
 
 <?php
-//验证登录用户名和密码
-if($this->input->post('user_email') && $this->input->post('user_password')){
-	$user_email = $this->input->post('user_email');
-	$user_password = md5($this->input->post('user_password'));
+session_start();
+if ($this->input->post('user_email'))
+{
+        if ($_SESSION['captcha'] != $this->input->post('captcha'))
+        {
+              echo "验证码不正确!";  
+        }
 
-	$query = $this->db->get_where('admin', array('user_email' => $user_email,'user_password' => $user_password));echo $user_password;
-	$result = $query->result();
+        //验证登录用户名和密码
+        else
+        if($this->input->post('user_email') && $this->input->post('user_password')){
+                $user_email = $this->input->post('user_email');
+                $user_password = md5($this->input->post('user_password'));
 
-	if(empty($result)){
-		echo '用户密码错误！';
-		echo site_url();
-	}else{
-        $this->input->set_cookie('user_email', $user_email, 60*60*24*365);
-        $this->input->set_cookie('user_password', $user_password, 60*60*24*365);
-	    //跳转
-		Header("HTTP/1.1 303 See Other");
-		Header("Location: ".site_url('admin'));
-		exit;
-	}
+                $query = $this->db->get_where('admin', array('user_email' => $user_email,'user_password' => $user_password));echo $user_password;
+                $result = $query->result();
+
+                if(empty($result)){
+                        echo '用户密码错误！';
+                        echo site_url();
+                }else{
+                $this->input->set_cookie('user_email', $user_email, 60*60*24*365);
+                $this->input->set_cookie('user_password', $user_password, 60*60*24*365);
+                    //跳转
+                        Header("HTTP/1.1 303 See Other");
+                        Header("Location: ".site_url('admin'));
+                        exit;
+                }
+        }
 }
 ?>
 
 <form action="<?php echo site_url('login')?>" method="post">
 <p><input name="user_email" type="text" placeholder="Email" /></p>
 <p><input name="user_password" type="password" placeholder="密码" /></p>
-
+<p>
+<img src="../../../captcha/captcha.php" id="captcha" />
+<a href="#" onclick="
+document.getElementById('captcha').src='../../../captcha/captcha.php?'+Math.random();
+document.getElementById('captcha-form').focus();"
+id="change-image">看不清?换一张.</a><br/><br/>
+<b>验证码:</b><br/>
+<input type="text" name="captcha" id="captcha-form" />
+</p>
 <p><input type="submit" value="登录" class="btn btn-success"></p>
 </form>
 
